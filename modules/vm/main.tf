0da209740cea0530f007f3ee6a60e78830c1eba4
advis-group <wealthadvis@gmail.com>
2026-04-30 18:02:56 +0530

    Add Azure VM resources in main.tf

---
+++ modules/vm/main.tf
@@ -0,0 +1,46 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
+  location            = var.location
+  resource_group_name = var.rg_name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = var.location
+  resource_group_name = var.rg_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-demo"
+  resource_group_name = var.rg_name
+  location            = var.location
+  size                = "Standard_B1s"
+
+  admin_username      = "azureuser"
+  admin_password      = "Password@123"
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+}
