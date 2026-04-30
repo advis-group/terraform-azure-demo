e0590a44f8145f6038460b8cad6f5946242079a1
advis-group <wealthadvis@gmail.com>
2026-04-30 18:20:35 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,26 +1,30 @@
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-demo"
-  resource_group_name = var.rg_name
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet"
   location            = var.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  admin_password      = "Password@123"
+  resource_group_name = var.rg_name
+  address_space       = ["10.0.0.0/16"]
+}
 
-  disable_password_authentication = false
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = var.rg_name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
 
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = var.location
+  resource_group_name = var.rg_name
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
